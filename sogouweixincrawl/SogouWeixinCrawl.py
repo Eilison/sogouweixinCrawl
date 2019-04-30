34e726cd8bcaf4b2edb12cfa24c44213acf6da85
ELtech <1277886419@qq.com>
2019-04-30 16:40:05 +0800

    完善搜狗搜索的时候的验证码处理逻辑

--- sogouweixincrawl/SogouWeixinCrawl.py
+++ sogouweixincrawl/SogouWeixinCrawl.py
@@ -17,33 +17,42 @@ class SogouWeixinCrawl(object):
     __keyword = ""
     __accountList = dict()
 
-    __sogouVerifyFunc = None
-    __weixinVerifyFunc = None
+    __verifyHandler = None
 
-    def __init__(self, sogouVerifyFunc=None, weixinVerifyFunc=None):
-        self.__sogouVerifyFunc = sogouVerifyFunc
-        self.__weixinVerifyFunc = weixinVerifyFunc
+    def __init__(self, sogouVerifyFunc=None, weixinVerifyFunc=None, screenSavePath="verifyscreenimg.png", verifyCodeSavePath="verifyimg.png"):
         self.__crawl = getBaseCrawler()
+
+        self.__verifyHandler = SogouWeixinVerify(self.__crawl,sogouVerifyFunc, weixinVerifyFunc, screenSavePath, verifyCodeSavePath)
 
     def getAccountList(self, keyword):
 
         if len(self.__accountList) > 0:
             return
 
+        mainUrl = "https://weixin.sogou.com/"
+        self.__crawl.getContent(mainUrl)
+        self.__crawl.waitLast(30)
         self.__keyword = keyword
-        self.keywordUrl = "http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8&s_from=input&_sug_=y&_sug_type_="%(self.__keyword)
 
-        self.__crawl.getContent(self.keywordUrl)
+        queryElement = self.__crawl.findElement(By.ID, "query")
+        if queryElement:
+            queryElement.send_keys(self.__keyword)
+            self.__crawl.findElement(By.XPATH, "//*[@uigs='search_account']").click()
+            self.__crawl.waitLast(30)
 
-        accountListElement = self.__crawl.findElement(By.CLASS_NAME, 'news-list2')
+            # self.keywordUrl = "http://weixin.sogou.com/weixin?type=1&query=%s&ie=utf8&s_from=input&_sug_=y&_sug_type_="%(self.__keyword)
 
-        if accountListElement is None:
-            SogouWeixinVerify().check(self.__crawl, self.__sogouVerifyFunc, self.__weixinVerifyFunc)
-            return
+            # self.__crawl.getContent(self.keywordUrl)
 
-        listHtml = accountListElement.get_attribute("innerHTML")
-        
-        self.__accountList = SogouWeixinAccountParser(listHtml).parser()
+            accountListElement = self.__crawl.findElement(By.CLASS_NAME, 'news-list2')
+
+            if accountListElement is None:
+                if not self.__verifyHandler.checkSogouVerify():
+                    return
+
+            listHtml = accountListElement.get_attribute("innerHTML")
+            
+            self.__accountList = SogouWeixinAccountParser(listHtml).parser()
 
         return self.__accountList
     
@@ -58,6 +67,9 @@ class SogouWeixinCrawl(object):
             self.__crawl.clickElement(self.__crawl.findElement(By.XPATH, '//a[@uigs="%s"]'%(nameUigs['nameUigs'])), 
                 1, 30)
 
+            if not self.__verifyHandler.checkWeixinVerify():
+                return
+
             pageContent = self.__crawl.getBrowser().page_source
 
             pattern = re.compile(r"var\s{1}msgList\s?=\s?[^\<\>]+};", re.I | re.M)
@@ -70,6 +82,9 @@ class SogouWeixinCrawl(object):
 
     def getArticleContent(self, url):
         self.__crawl.getContent(url)
+
+        if not self.__verifyHandler.checkWeixinVerify():
+                return
 
         contentElement = self.__crawl.findElement(By.XPATH, "//div[@id='img-content']")
         if contentElement:
@@ -87,6 +102,9 @@ class SogouWeixinCrawl(object):
         if articleUigs:
             self.__crawl.clickElement(self.__crawl.findElement(By.XPATH, '//a[@uigs="%s"]'%(articleUigs['recentArticleUigs'])), 
                 1, 30)
+
+            if not self.__verifyHandler.checkWeixinVerify():
+                return
 
         return self.__crawl.getBrowser().page_source
 
--- sogouweixincrawl/SogouWeixinVerify.py
+++ sogouweixincrawl/SogouWeixinVerify.py
@@ -1,43 +1,108 @@
 
 from lxml import etree
 from PIL import Image
+from selenium.webdriver.common.by import By
 
 class SogouWeixinVerify(object):
 
-    def check(self, crawl=None, sogouVerifyFunc=None, weixinVerifyFunc=None):
-        if crawl is None:
-            return
-        
-        pageHtml = etree.HTML(crawl.page_source, etree.HTMLParser())
+    __crawl = None
 
-        sogouVerifyElement = pageHtml.xpath('//*[@id="seccodeImage"]')
+    __screenSavePath = ""
+    __verifyCodeSavePath = ""
+    __keepVerify = True
+
+    __sogouVerifyFunc = None
+    __weixinVerifyFunc = None
+
+    def __init__(self, crawl=None, 
+        sogouVerifyFunc=None, 
+        weixinVerifyFunc=None, 
+        screenSavePath=None, 
+        verifyCodeSavePath=None, 
+        keepVerify=True):
+
+        self.__crawl = crawl
+        self.__sogouVerifyFunc = sogouVerifyFunc
+        self.__weixinVerifyFunc = weixinVerifyFunc
+        self.__screenSavePath = screenSavePath
+        self.__verifyCodeSavePath = verifyCodeSavePath
+        self.__keepVerify = keepVerify
+
+    def checkSogouVerify(self):
+        if self.__crawl is None:
+            assert self.__crawl is not None
+        
+        # pageHtml = etree.HTML(crawl.getBrowser().page_source, etree.HTMLParser())
+        sogouVerifyElement = self.__crawl.findElement(By.XPATH, '//*[@id="seccodeImage"]')
         
         if sogouVerifyElement:
             
-            if callable(sogouVerifyFunc):
-                sogouVerifyFunc(crawl, self.getVerifyImg(crawl, sogouVerifyElement))
-            return
-        
-        weixinVerifyElement = pageHtml.xpath('//*[@class="weui_input"]')
-        
+            if callable(self.__sogouVerifyFunc):
+                code = self.__sogouVerifyFunc(self.__crawl, self.getVerifyImg(sogouVerifyElement))
+                if code:
+                    return self.inputSogouVerifyCode(code)
+                    
+            return False
+        else:
+            return True
+
+    def checkWeixinVerify(self):
+        if self.__crawl is None:
+            assert self.__crawl is not None
+
+        contentElement = self.__crawl.findElement(By.ID, "img-content")
+
+        if contentElement:
+            return True
+        else:
+            contentElement = self.__crawl.findElement(By.ID, "js_content")
+            if contentElement:
+                return True
+
+        weixinVerifyElement = self.__crawl.findElement(By.XPATH,'//*[@class="weui_input"]')
+            
         if weixinVerifyElement:
             
-            if callable(weixinVerifyFunc):
-                weixinVerifyFunc(crawl, self.getVerifyImg(crawl, weixinVerifyElement))
-            return
+            if callable(self.__weixinVerifyFunc):
+                code = self.__weixinVerifyFunc(self.__crawl, self.getVerifyImg(weixinVerifyElement))
+                if code:
+                    return self.inputWeixinVerifyCode(code)
+            return False
+        else:
+            return True
 
-    def getVerifyImg(self, crawl, element):
+    def getVerifyImg(self, element):
         location = element.location
         size = element.size
 
-        crawl.getBrowser().save_screenshot("screenimg.png")
+        if self.__screenSavePath:
+            self.__crawl.getBrowser().save_screenshot(self.__screenSavePath)
 
         x = location['x']
         y = location['y']
-        width = location['x']+size['width']
-        height = location['y']+size['height']
+        width = location['x'] + size['width']
+        height = location['y'] + size['height']
 
-        im = Image.open('screenimg.png')
+        im = Image.open(self.__screenSavePath)
         im = im.crop((int(x), int(y), int(width), int(height)))
-        im.save('verifyimg.png')
+        if self.__verifyCodeSavePath:
+            im.save(self.__verifyCodeSavePath)
         return im
+
+    def inputSogouVerifyCode(self, code):
+        verifyInput = self.__crawl.findElement(By.ID, "seccodeInput")
+        verifyInput.send_keys(code)
+        self.__crawl.findElement(By.ID, "submit").click()
+        sogouVerifyElement = self.__crawl.findElement(By.XPATH, '//*[@id="seccodeImage"]')
+        self.__crawl.waitLast(30)
+
+        if sogouVerifyElement:
+            if not self.__keepVerify:
+                return False
+            else:
+                self.checkSogouVerify()
+        else:
+            return True
+
+    def inputWeixinVerifyCode(self, code):
+        pass
